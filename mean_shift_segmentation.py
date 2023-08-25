e3dec244e39cb29bd6d1e45de6d599812e6e0c0f
astroboy890506 <97178858+astroboy890506@users.noreply.github.com>
2023-08-25 19:44:43 +0800

    Update mean_shift_segmentation.py

--- mean_shift_segmentation.py
+++ mean_shift_segmentation.py
@@ -3,35 +3,14 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-def kmeans_segmentation(img_path, num_clusters):
+def mean_shift_segmentation(img_path, spatial_radius, color_radius, min_density):
     # Load the image
     img = cv2.imread(img_path)
 
-    # Reshape the image to a 2D array of pixels
-    pixels = img.reshape((-1, 3))  # Modify to (-1, 3) if color image
+    # Apply mean shift clustering
+    shifted = cv2.pyrMeanShiftFiltering(img, spatial_radius, color_radius, min_density)
 
-    # Convert to float32
-    pixels = np.float32(pixels)
-
-    # Define the criteria and flags for k-means
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 1.0)
-    k = num_clusters  # Number of clusters
-    flags = cv2.KMEANS_RANDOM_CENTERS
-
-    # Apply k-means clustering
-    ret, label, center = cv2.kmeans(pixels, k, None, criteria, 20, flags)  # Increase the iteration count
-
-    # Convert the center values back to uint8
-    center = np.uint8(center)
-
-    # Separate pixels based on their labels (clusters)
-    segmented_imgs = [np.zeros_like(img) for _ in range(k)]
-
-    for i in range(k):
-        cluster_mask = (label == i).reshape(img.shape[:2])
-        segmented_imgs[i][cluster_mask] = img[cluster_mask]
-
-    return img, segmented_imgs
+    return img, shifted
 
 def main():
     st.set_page_config(
@@ -41,33 +20,28 @@ def main():
         initial_sidebar_state="expanded"
     )
 
-    st.title("Image Segmentation using K-Means")
+    st.title("Image Segmentation using Mean Shift")
 
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
-        num_clusters = st.slider("Number of Clusters (k)", min_value=2, max_value=10, value=5)
+        spatial_radius = st.slider("Spatial Radius", min_value=1, max_value=100, value=20)
+        color_radius = st.slider("Color Radius", min_value=1, max_value=100, value=30)
+        min_density = st.slider("Minimum Density", min_value=1, max_value=100, value=10)
 
         img_path = "uploaded_image.jpg"
         with open(img_path, "wb") as f:
             f.write(uploaded_file.read())
 
-        original_img, segmented_imgs = kmeans_segmentation(img_path, num_clusters)
+        original_img, segmented_img = mean_shift_segmentation(img_path, spatial_radius, color_radius, min_density)
 
         image_width = st.slider("Adjust Image Width", min_value=100, max_value=800, value=500)
 
         st.subheader("Original Image")
         st.image(cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB), width=image_width)
 
-        col1, col2 = st.beta_columns(2)
-
-        for i in range(num_clusters):
-            if i % 2 == 0:
-                col1.subheader(f"Cluster {i + 1}")
-                col1.image(cv2.cvtColor(segmented_imgs[i], cv2.COLOR_BGR2RGB), width=image_width)
-            else:
-                col2.subheader(f"Cluster {i + 1}")
-                col2.image(cv2.cvtColor(segmented_imgs[i], cv2.COLOR_BGR2RGB), width=image_width)
+        st.subheader("Segmented Image")
+        st.image(cv2.cvtColor(segmented_img, cv2.COLOR_BGR2RGB), width=image_width)
 
 if __name__ == "__main__":
     main()
